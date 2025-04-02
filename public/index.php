0088718ebb957407b829ce3f1203327af7da53cd
Steven Portas <stevenportas@me.com>
2025-04-02 11:24:26 -0700

    Update index.php to render the correct Twig template path

--- public/index.php
+++ public/index.php
@@ -1,4 +1,5 @@
+<?php
 require_once __DIR__ . '/../vendor/autoload.php';
 $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../src/templates');
 $twig = new \Twig\Environment($loader);
-echo $twig->render('index.twig', ['title' => 'Photosnap']);
+echo $twig->render('pages/index.twig', ['title' => 'Photosnap']);
