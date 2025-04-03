3fcb433e205dc137cc96389bc3cbe6161141cd90
Steven Portas <stevenportas@me.com>
2025-04-03 09:37:11 -0700

    Refactor feature table styles and markup for improved responsiveness and accessibility
    - Updated SCSS for the feature table component to enhance layout and styling, including responsive adjustments using breakpoints.
    - Modified the HTML structure in the Twig template to ensure proper semantic markup and accessibility features.
    - Improved the visibility of features in mobile view by adjusting the display of table cells and headers.
    - Ensured consistent styling for table elements and added necessary media queries for better adaptability across devices.

--- cache/06/06388df36083fe98d70b7db0a9a2290d.php
+++ cache/06/06388df36083fe98d70b7db0a9a2290d.php
@@ -41,30 +41,30 @@ class __TwigTemplate_458783cd27b34eb7728ed57ac3db4304 extends Template
         // line 6
         yield "
 <div class=\"feature-table\" role=\"region\" aria-label=\"Feature comparison\">
-  <table>
-    <caption class=\"visually-hidden\">Compare features across different plans</caption>
-    <thead>
-      <tr>
-        <th scope=\"col\">THE FEATURES</th>
-        ";
+\t<table>
+\t\t<caption class=\"visually-hidden\">Compare features across different plans</caption>
+\t\t<thead>
+\t\t\t<tr>
+\t\t\t\t<th scope=\"col\">THE FEATURES</th>
+\t\t\t\t";
         // line 13
         $context['_parent'] = $context;
         $context['_seq'] = CoreExtension::ensureTraversable(($context["plans"] ?? null));
         foreach ($context['_seq'] as $context["_key"] => $context["plan"]) {
             // line 14
-            yield "          <th scope=\"col\">";
+            yield "\t\t\t\t\t<th scope=\"col\">";
             yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(CoreExtension::getAttribute($this->env, $this->source, $context["plan"], "name", [], "any", false, false, false, 14), "html", null, true);
             yield "</th>
-        ";
+\t\t\t\t";
         }
         $_parent = $context['_parent'];
         unset($context['_seq'], $context['_key'], $context['plan'], $context['_parent']);
         $context = array_intersect_key($context, $_parent) + $_parent;
         // line 16
-        yield "      </tr>
-    </thead>
-    <tbody>
-      ";
+        yield "\t\t\t</tr>
+\t\t</thead>
+\t\t<tbody>
+\t\t\t";
         // line 19
         $context['_parent'] = $context;
         $context['_seq'] = CoreExtension::ensureTraversable(($context["features"] ?? null));
@@ -83,12 +83,12 @@ class __TwigTemplate_458783cd27b34eb7728ed57ac3db4304 extends Template
         }
         foreach ($context['_seq'] as $context["_key"] => $context["feature"]) {
             // line 20
-            yield "        <tr>
-          <th scope=\"row\">";
+            yield "\t\t\t\t<tr>
+\t\t\t\t\t<th scope=\"row\">";
             // line 21
             yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(CoreExtension::getAttribute($this->env, $this->source, $context["feature"], "title", [], "any", false, false, false, 21), "html", null, true);
             yield "</th>
-          ";
+\t\t\t\t\t";
             // line 22
             $context['_parent'] = $context;
             $context['_seq'] = CoreExtension::ensureTraversable(($context["plans"] ?? null));
@@ -107,35 +107,34 @@ class __TwigTemplate_458783cd27b34eb7728ed57ac3db4304 extends Template
             }
             foreach ($context['_seq'] as $context["_key"] => $context["plan"]) {
                 // line 23
-                yield "            <td>
-              <div>";
+                yield "\t\t\t\t\t\t<td>
+\t\t\t\t\t\t\t<div>";
                 // line 24
                 yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(CoreExtension::getAttribute($this->env, $this->source, $context["plan"], "name", [], "any", false, false, false, 24), "html", null, true);
                 yield "</div>
-              ";
+\t\t\t\t\t\t\t";
                 // line 25
                 if ((($_v0 = CoreExtension::getAttribute($this->env, $this->source, $context["plan"], "features", [], "any", false, false, false, 25)) && is_array($_v0) || $_v0 instanceof ArrayAccess ? ($_v0[CoreExtension::getAttribute($this->env, $this->source, CoreExtension::getAttribute($this->env, $this->source, CoreExtension::getAttribute($this->env, $this->source, $context["loop"], "parent", [], "any", false, false, false, 25), "loop", [], "any", false, false, false, 25), "index0", [], "any", false, false, false, 25)] ?? null) : null)) {
                     // line 26
-                    yield "                <img src=\"/pricing/desktop/check.svg\"
-                     alt=\"";
-                    // line 27
-                    yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(CoreExtension::getAttribute($this->env, $this->source, $context["feature"], "title", [], "any", false, false, false, 27), "html", null, true);
+                    yield "\t\t\t\t\t\t\t\t<img src=\"images/pricing/desktop/check.svg\" alt=\"";
+                    yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(CoreExtension::getAttribute($this->env, $this->source, $context["feature"], "title", [], "any", false, false, false, 26), "html", null, true);
                     yield " is included in ";
-                    yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(CoreExtension::getAttribute($this->env, $this->source, $context["plan"], "name", [], "any", false, false, false, 27), "html", null, true);
-                    yield " plan\"
-                     width=\"16\"
-                     height=\"12\" />
-              ";
+                    yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(CoreExtension::getAttribute($this->env, $this->source, $context["plan"], "name", [], "any", false, false, false, 26), "html", null, true);
+                    yield " plan\" width=\"16\" height=\"12\"/>
+\t\t\t\t\t\t\t";
                 } else {
-                    // line 31
-                    yield "                <span class=\"visually-hidden\">Not included in ";
-                    yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(CoreExtension::getAttribute($this->env, $this->source, $context["plan"], "name", [], "any", false, false, false, 31), "html", null, true);
-                    yield " plan</span>
-              ";
+                    // line 28
+                    yield "\t\t\t\t\t\t\t\t<span class=\"visually-hidden\">Not included in
+\t\t\t\t\t\t\t\t\t";
+                    // line 29
+                    yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(CoreExtension::getAttribute($this->env, $this->source, $context["plan"], "name", [], "any", false, false, false, 29), "html", null, true);
+                    yield "
+\t\t\t\t\t\t\t\t\tplan</span>
+\t\t\t\t\t\t\t";
                 }
-                // line 33
-                yield "            </td>
-          ";
+                // line 32
+                yield "\t\t\t\t\t\t</td>
+\t\t\t\t\t";
                 ++$context['loop']['index0'];
                 ++$context['loop']['index'];
                 $context['loop']['first'] = false;
@@ -148,9 +147,9 @@ class __TwigTemplate_458783cd27b34eb7728ed57ac3db4304 extends Template
             $_parent = $context['_parent'];
             unset($context['_seq'], $context['_key'], $context['plan'], $context['_parent'], $context['loop']);
             $context = array_intersect_key($context, $_parent) + $_parent;
-            // line 35
-            yield "        </tr>
-      ";
+            // line 34
+            yield "\t\t\t\t</tr>
+\t\t\t";
             ++$context['loop']['index0'];
             ++$context['loop']['index'];
             $context['loop']['first'] = false;
@@ -163,10 +162,11 @@ class __TwigTemplate_458783cd27b34eb7728ed57ac3db4304 extends Template
         $_parent = $context['_parent'];
         unset($context['_seq'], $context['_key'], $context['feature'], $context['_parent'], $context['loop']);
         $context = array_intersect_key($context, $_parent) + $_parent;
-        // line 37
-        yield "    </tbody>
-  </table>
-</div>";
+        // line 36
+        yield "\t\t</tbody>
+\t</table>
+</div>
+";
         yield from [];
     }
 
@@ -191,7 +191,7 @@ class __TwigTemplate_458783cd27b34eb7728ed57ac3db4304 extends Template
      */
     public function getDebugInfo(): array
     {
-        return array (  167 => 37,  152 => 35,  137 => 33,  131 => 31,  122 => 27,  119 => 26,  117 => 25,  113 => 24,  110 => 23,  93 => 22,  89 => 21,  86 => 20,  69 => 19,  64 => 16,  55 => 14,  51 => 13,  42 => 6,);
+        return array (  166 => 36,  151 => 34,  136 => 32,  130 => 29,  127 => 28,  119 => 26,  117 => 25,  113 => 24,  110 => 23,  93 => 22,  89 => 21,  86 => 20,  69 => 19,  64 => 16,  55 => 14,  51 => 13,  42 => 6,);
     }
 
     public function getSourceContext(): Source
@@ -203,37 +203,37 @@ class __TwigTemplate_458783cd27b34eb7728ed57ac3db4304 extends Template
 #}
 
 <div class=\"feature-table\" role=\"region\" aria-label=\"Feature comparison\">
-  <table>
-    <caption class=\"visually-hidden\">Compare features across different plans</caption>
-    <thead>
-      <tr>
-        <th scope=\"col\">THE FEATURES</th>
-        {% for plan in plans %}
-          <th scope=\"col\">{{ plan.name }}</th>
-        {% endfor %}
-      </tr>
-    </thead>
-    <tbody>
-      {% for feature in features %}
-        <tr>
-          <th scope=\"row\">{{ feature.title }}</th>
-          {% for plan in plans %}
-            <td>
-              <div>{{ plan.name }}</div>
-              {% if plan.features[loop.parent.loop.index0] %}
-                <img src=\"/pricing/desktop/check.svg\"
-                     alt=\"{{ feature.title }} is included in {{ plan.name }} plan\"
-                     width=\"16\"
-                     height=\"12\" />
-              {% else %}
-                <span class=\"visually-hidden\">Not included in {{ plan.name }} plan</span>
-              {% endif %}
-            </td>
-          {% endfor %}
-        </tr>
-      {% endfor %}
-    </tbody>
-  </table>
-</div>", "components/feature-table/feature-table.twig", "/Users/stevenportas/WorkBox/Sites/demos/demo-photosnap-twig/src/templates/components/feature-table/feature-table.twig");
+\t<table>
+\t\t<caption class=\"visually-hidden\">Compare features across different plans</caption>
+\t\t<thead>
+\t\t\t<tr>
+\t\t\t\t<th scope=\"col\">THE FEATURES</th>
+\t\t\t\t{% for plan in plans %}
+\t\t\t\t\t<th scope=\"col\">{{ plan.name }}</th>
+\t\t\t\t{% endfor %}
+\t\t\t</tr>
+\t\t</thead>
+\t\t<tbody>
+\t\t\t{% for feature in features %}
+\t\t\t\t<tr>
+\t\t\t\t\t<th scope=\"row\">{{ feature.title }}</th>
+\t\t\t\t\t{% for plan in plans %}
+\t\t\t\t\t\t<td>
+\t\t\t\t\t\t\t<div>{{ plan.name }}</div>
+\t\t\t\t\t\t\t{% if plan.features[loop.parent.loop.index0] %}
+\t\t\t\t\t\t\t\t<img src=\"images/pricing/desktop/check.svg\" alt=\"{{ feature.title }} is included in {{ plan.name }} plan\" width=\"16\" height=\"12\"/>
+\t\t\t\t\t\t\t{% else %}
+\t\t\t\t\t\t\t\t<span class=\"visually-hidden\">Not included in
+\t\t\t\t\t\t\t\t\t{{ plan.name }}
+\t\t\t\t\t\t\t\t\tplan</span>
+\t\t\t\t\t\t\t{% endif %}
+\t\t\t\t\t\t</td>
+\t\t\t\t\t{% endfor %}
+\t\t\t\t</tr>
+\t\t\t{% endfor %}
+\t\t</tbody>
+\t</table>
+</div>
+", "components/feature-table/feature-table.twig", "/Users/stevenportas/WorkBox/Sites/demos/demo-photosnap-twig/src/templates/components/feature-table/feature-table.twig");
     }
 }
