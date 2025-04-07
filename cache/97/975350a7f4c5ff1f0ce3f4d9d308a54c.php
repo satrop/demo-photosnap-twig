d1e9a63bfb3f9849a3b79df567d33ced826233ca
Steven Portas <stevenportas@me.com>
2025-04-07 13:25:36 -0700

    feat: Add isFirstSection parameter to hero component and update heading level dynamically

--- cache/97/975350a7f4c5ff1f0ce3f4d9d308a54c.php
+++ cache/97/975350a7f4c5ff1f0ce3f4d9d308a54c.php
@@ -38,33 +38,33 @@ class __TwigTemplate_6dc431587667dbd52804603acffa5546 extends Template
     protected function doDisplay(array $context, array $blocks = []): iterable
     {
         $macros = $this->macros;
-        // line 18
+        // line 19
         yield "
 ";
-        // line 19
-        $context["backgroundClass"] = (((($context["backgroundColor"] ?? null) == "black")) ? ("background--black") : ("background--white"));
         // line 20
-        $context["imagePositionClass"] = ("image-" . ((array_key_exists("imagePosition", $context)) ? (Twig\Extension\CoreExtension::default(($context["imagePosition"] ?? null), "right")) : ("right")));
+        $context["backgroundClass"] = (((($context["backgroundColor"] ?? null) == "black")) ? ("background--black") : ("background--white"));
         // line 21
-        $context["shortClass"] = ((($context["short"] ?? null)) ? ("short") : (""));
+        $context["imagePositionClass"] = ("image-" . ((array_key_exists("imagePosition", $context)) ? (Twig\Extension\CoreExtension::default(($context["imagePosition"] ?? null), "right")) : ("right")));
         // line 22
-        $context["classes"] = Twig\Extension\CoreExtension::trim(Twig\Extension\CoreExtension::join(["hero", ($context["variant"] ?? null), ($context["imagePositionClass"] ?? null), "full-width", ($context["backgroundClass"] ?? null), ($context["shortClass"] ?? null)], " "));
+        $context["shortClass"] = ((($context["short"] ?? null)) ? ("short") : (""));
         // line 23
+        $context["classes"] = Twig\Extension\CoreExtension::trim(Twig\Extension\CoreExtension::join(["hero", ($context["variant"] ?? null), ($context["imagePositionClass"] ?? null), "full-width", ($context["backgroundClass"] ?? null), ($context["shortClass"] ?? null)], " "));
+        // line 24
         yield "
 <section class=\"";
-        // line 24
+        // line 25
         yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(($context["classes"] ?? null), "html", null, true);
         yield "\" aria-labelledby=\"hero-title-";
         yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(Twig\Extension\CoreExtension::lower($this->env->getCharset(), Twig\Extension\CoreExtension::replace(($context["title"] ?? null), [" " => "-"])), "html", null, true);
         yield "\">
 \t<div class=\"imageContainer\" ";
-        // line 25
+        // line 26
         if ((($context["variant"] ?? null) == "overlay")) {
             yield " aria-hidden=\"true\" ";
         }
         yield ">
 \t\t<img src=\"";
-        // line 26
+        // line 27
         yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(($context["image"] ?? null), "html", null, true);
         yield "\" alt=\"";
         yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(($context["imageAlt"] ?? null), "html", null, true);
@@ -73,24 +73,24 @@ class __TwigTemplate_6dc431587667dbd52804603acffa5546 extends Template
 
 \t<div class=\"content\">
 \t\t";
-        // line 30
+        // line 31
         if (($context["standout"] ?? null)) {
-            // line 31
+            // line 32
             yield "\t\t\t<div class=\"standout-content\" role=\"presentation\">
 \t\t\t\t";
-            // line 32
-            yield $this->getTemplateForMacro("macro_content_block", $context, 32, $this->getSourceContext())->macro_content_block(...[$context]);
+            // line 33
+            yield $this->getTemplateForMacro("macro_content_block", $context, 33, $this->getSourceContext())->macro_content_block(...[$context]);
             yield "
 \t\t\t</div>
 \t\t";
         } else {
-            // line 35
+            // line 36
             yield "\t\t\t";
-            yield $this->getTemplateForMacro("macro_content_block", $context, 35, $this->getSourceContext())->macro_content_block(...[$context]);
+            yield $this->getTemplateForMacro("macro_content_block", $context, 36, $this->getSourceContext())->macro_content_block(...[$context]);
             yield "
 \t\t";
         }
-        // line 37
+        // line 38
         yield "\t</div>
 </section>
 
@@ -98,7 +98,7 @@ class __TwigTemplate_6dc431587667dbd52804603acffa5546 extends Template
         yield from [];
     }
 
-    // line 40
+    // line 41
     public function macro_content_block($params = null, ...$varargs): string|Markup
     {
         $macros = $this->macros;
@@ -110,80 +110,91 @@ class __TwigTemplate_6dc431587667dbd52804603acffa5546 extends Template
         $blocks = [];
 
         return ('' === $tmp = \Twig\Extension\CoreExtension::captureOutput((function () use (&$context, $macros, $blocks) {
-            // line 41
+            // line 42
             yield "\t";
-            if (CoreExtension::getAttribute($this->env, $this->source, ($context["params"] ?? null), "eyebrow", [], "any", false, false, false, 41)) {
-                // line 42
+            if (CoreExtension::getAttribute($this->env, $this->source, ($context["params"] ?? null), "eyebrow", [], "any", false, false, false, 42)) {
+                // line 43
                 yield "\t\t<p class=\"eyebrow h4\" aria-level=\"2\" role=\"heading\">";
-                yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(CoreExtension::getAttribute($this->env, $this->source, ($context["params"] ?? null), "eyebrow", [], "any", false, false, false, 42), "html", null, true);
+                yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(CoreExtension::getAttribute($this->env, $this->source, ($context["params"] ?? null), "eyebrow", [], "any", false, false, false, 43), "html", null, true);
                 yield "</p>
 \t";
             }
-            // line 44
-            yield "
-\t<h1 id=\"hero-title-";
             // line 45
-            yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(Twig\Extension\CoreExtension::lower($this->env->getCharset(), Twig\Extension\CoreExtension::replace(CoreExtension::getAttribute($this->env, $this->source, ($context["params"] ?? null), "title", [], "any", false, false, false, 45), [" " => "-"])), "html", null, true);
-            yield "\" class=\"title\">";
-            yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(CoreExtension::getAttribute($this->env, $this->source, ($context["params"] ?? null), "title", [], "any", false, false, false, 45), "html", null, true);
-            yield "</h1>
+            yield "
+\t";
+            // line 46
+            $context["headingLevel"] = ((((CoreExtension::getAttribute($this->env, $this->source, ($context["params"] ?? null), "isFirstSection", [], "any", true, true, false, 46)) ? (Twig\Extension\CoreExtension::default(CoreExtension::getAttribute($this->env, $this->source, ($context["params"] ?? null), "isFirstSection", [], "any", false, false, false, 46), false)) : (false))) ? ("h1") : ("h2"));
+            // line 47
+            yield "\t<";
+            yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(($context["headingLevel"] ?? null), "html", null, true);
+            yield " id=\"hero-title-";
+            yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(Twig\Extension\CoreExtension::lower($this->env->getCharset(), Twig\Extension\CoreExtension::replace(CoreExtension::getAttribute($this->env, $this->source, ($context["params"] ?? null), "title", [], "any", false, false, false, 47), [" " => "-"])), "html", null, true);
+            yield "\" class=\"title ";
+            if ( !CoreExtension::getAttribute($this->env, $this->source, ($context["params"] ?? null), "isFirstSection", [], "any", false, false, false, 47)) {
+                yield "h1";
+            }
+            yield "\">";
+            yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(CoreExtension::getAttribute($this->env, $this->source, ($context["params"] ?? null), "title", [], "any", false, false, false, 47), "html", null, true);
+            yield "</";
+            yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(($context["headingLevel"] ?? null), "html", null, true);
+            yield ">
 
 \t";
-            // line 47
-            if ((CoreExtension::getAttribute($this->env, $this->source, ($context["params"] ?? null), "date", [], "any", false, false, false, 47) || CoreExtension::getAttribute($this->env, $this->source, ($context["params"] ?? null), "author", [], "any", false, false, false, 47))) {
-                // line 48
+            // line 49
+            if ((CoreExtension::getAttribute($this->env, $this->source, ($context["params"] ?? null), "date", [], "any", false, false, false, 49) || CoreExtension::getAttribute($this->env, $this->source, ($context["params"] ?? null), "author", [], "any", false, false, false, 49))) {
+                // line 50
                 yield "\t\t<div class=\"meta\">
 \t\t\t";
-                // line 49
-                if (CoreExtension::getAttribute($this->env, $this->source, ($context["params"] ?? null), "date", [], "any", false, false, false, 49)) {
-                    // line 50
+                // line 51
+                if (CoreExtension::getAttribute($this->env, $this->source, ($context["params"] ?? null), "date", [], "any", false, false, false, 51)) {
+                    // line 52
                     yield "\t\t\t\t<time class=\"date\">";
-                    yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(CoreExtension::getAttribute($this->env, $this->source, ($context["params"] ?? null), "date", [], "any", false, false, false, 50), "html", null, true);
+                    yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(CoreExtension::getAttribute($this->env, $this->source, ($context["params"] ?? null), "date", [], "any", false, false, false, 52), "html", null, true);
                     yield "</time>
 \t\t\t";
                 }
-                // line 52
+                // line 54
                 yield "\t\t\t";
-                if (CoreExtension::getAttribute($this->env, $this->source, ($context["params"] ?? null), "author", [], "any", false, false, false, 52)) {
-                    // line 53
+                if (CoreExtension::getAttribute($this->env, $this->source, ($context["params"] ?? null), "author", [], "any", false, false, false, 54)) {
+                    // line 55
                     yield "\t\t\t\t<span class=\"author\">
 \t\t\t\t\t<span class=\"visually-hidden\">Article written
 \t\t\t\t\t</span>
 \t\t\t\t\tby
 \t\t\t\t\t";
-                    // line 57
-                    yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(CoreExtension::getAttribute($this->env, $this->source, ($context["params"] ?? null), "author", [], "any", false, false, false, 57), "html", null, true);
+                    // line 59
+                    yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(CoreExtension::getAttribute($this->env, $this->source, ($context["params"] ?? null), "author", [], "any", false, false, false, 59), "html", null, true);
                     yield "
 \t\t\t\t</span>
 \t\t\t";
                 }
-                // line 60
+                // line 62
                 yield "\t\t</div>
 \t";
             }
-            // line 62
+            // line 64
             yield "
 \t";
-            // line 63
-            if (CoreExtension::getAttribute($this->env, $this->source, ($context["params"] ?? null), "excerpt", [], "any", false, false, false, 63)) {
-                // line 64
+            // line 65
+            if (CoreExtension::getAttribute($this->env, $this->source, ($context["params"] ?? null), "excerpt", [], "any", false, false, false, 65)) {
+                // line 66
                 yield "\t\t<p class=\"excerpt\">";
-                yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(CoreExtension::getAttribute($this->env, $this->source, ($context["params"] ?? null), "excerpt", [], "any", false, false, false, 64), "html", null, true);
+                yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(CoreExtension::getAttribute($this->env, $this->source, ($context["params"] ?? null), "excerpt", [], "any", false, false, false, 66), "html", null, true);
                 yield "</p>
 \t";
             }
-            // line 66
+            // line 68
             yield "
 \t";
-            // line 67
-            if (CoreExtension::getAttribute($this->env, $this->source, ($context["params"] ?? null), "buttonText", [], "any", false, false, false, 67)) {
-                // line 68
+            // line 69
+            if (CoreExtension::getAttribute($this->env, $this->source, ($context["params"] ?? null), "buttonText", [], "any", false, false, false, 69)) {
+                // line 70
                 yield "\t\t";
-                yield from $this->loadTemplate("components/button/button.twig", "components/hero/hero.twig", 68)->unwrap()->yield(CoreExtension::merge($context, ["text" => CoreExtension::getAttribute($this->env, $this->source,                 // line 69
-($context["params"] ?? null), "buttonText", [], "any", false, false, false, 69), "href" => CoreExtension::getAttribute($this->env, $this->source,                 // line 70
-($context["params"] ?? null), "buttonHref", [], "any", false, false, false, 70), "variant" => "secondary", "ariaLabel" => ((CoreExtension::getAttribute($this->env, $this->source,                 // line 72
-($context["params"] ?? null), "buttonText", [], "any", false, false, false, 72) . " - ") . CoreExtension::getAttribute($this->env, $this->source, ($context["params"] ?? null), "title", [], "any", false, false, false, 72))]));
-                // line 74
+                yield from $this->loadTemplate("components/button/button.twig", "components/hero/hero.twig", 70)->unwrap()->yield(CoreExtension::merge($context, ["text" => CoreExtension::getAttribute($this->env, $this->source,                 // line 71
+($context["params"] ?? null), "buttonText", [], "any", false, false, false, 71), "href" => CoreExtension::getAttribute($this->env, $this->source,                 // line 72
+($context["params"] ?? null), "buttonHref", [], "any", false, false, false, 72), "variant" => "secondary", "ariaLabel" => ((CoreExtension::getAttribute($this->env, $this->source,                 // line 74
+($context["params"] ?? null), "buttonText", [], "any", false, false, false, 74) . " - ") . CoreExtension::getAttribute($this->env, $this->source, ($context["params"] ?? null), "title", [], "any", false, false, false, 74))]));
+                // line 76
                 yield "\t";
             }
             yield from [];
@@ -211,7 +222,7 @@ class __TwigTemplate_6dc431587667dbd52804603acffa5546 extends Template
      */
     public function getDebugInfo(): array
     {
-        return array (  187 => 74,  185 => 72,  184 => 70,  183 => 69,  181 => 68,  179 => 67,  176 => 66,  170 => 64,  168 => 63,  165 => 62,  161 => 60,  155 => 57,  149 => 53,  146 => 52,  140 => 50,  138 => 49,  135 => 48,  133 => 47,  126 => 45,  123 => 44,  117 => 42,  114 => 41,  102 => 40,  94 => 37,  88 => 35,  82 => 32,  79 => 31,  77 => 30,  68 => 26,  62 => 25,  56 => 24,  53 => 23,  51 => 22,  49 => 21,  47 => 20,  45 => 19,  42 => 18,);
+        return array (  198 => 76,  196 => 74,  195 => 72,  194 => 71,  192 => 70,  190 => 69,  187 => 68,  181 => 66,  179 => 65,  176 => 64,  172 => 62,  166 => 59,  160 => 55,  157 => 54,  151 => 52,  149 => 51,  146 => 50,  144 => 49,  128 => 47,  126 => 46,  123 => 45,  117 => 43,  114 => 42,  102 => 41,  94 => 38,  88 => 36,  82 => 33,  79 => 32,  77 => 31,  68 => 27,  62 => 26,  56 => 25,  53 => 24,  51 => 23,  49 => 22,  47 => 21,  45 => 20,  42 => 19,);
     }
 
     public function getSourceContext(): Source
@@ -232,6 +243,7 @@ class __TwigTemplate_6dc431587667dbd52804603acffa5546 extends Template
   @param {string} backgroundColor - Background color (black|white)
   @param {boolean} standout - Whether to show standout content
   @param {boolean} short - Whether to show short version
+  @param {boolean} isFirstSection - Whether this is the first section on the page
 #}
 
 {% set backgroundClass = backgroundColor == 'black' ? 'background--black' : 'background--white' %}
@@ -260,7 +272,8 @@ class __TwigTemplate_6dc431587667dbd52804603acffa5546 extends Template
 \t\t<p class=\"eyebrow h4\" aria-level=\"2\" role=\"heading\">{{ params.eyebrow }}</p>
 \t{% endif %}
 
-\t<h1 id=\"hero-title-{{ params.title|replace({' ': '-'})|lower }}\" class=\"title\">{{ params.title }}</h1>
+\t{% set headingLevel = params.isFirstSection|default(false) ? 'h1' : 'h2' %}
+\t<{{ headingLevel }} id=\"hero-title-{{ params.title|replace({' ': '-'})|lower }}\" class=\"title {% if not params.isFirstSection %}h1{% endif %}\">{{ params.title }}</{{ headingLevel }}>
 
 \t{% if params.date or params.author %}
 \t\t<div class=\"meta\">
